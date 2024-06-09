3603f589a7dddff57b52b3057e17e902d82e135d
Krasimir Mladenov <kdmladenov@yahoo.com>
2024-06-09 18:35:16 +0300

    Updated clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,25 @@
-import { authMiddleware, redirectToSignIn } from '@clerk/nextjs/server';
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 
+// See https://clerk.com/docs/references/nextjs/auth-middleware
 export default authMiddleware({
   async afterAuth(auth, request) {
+    // if users are not authenticated
     if (!auth.userId && !auth.isPublicRoute) {
       return redirectToSignIn({ returnBackUrl: request.url });
     }
+
+    // If user has signed up, register on Stream backend
+    if (auth.userId && !auth.user?.privateMetadata?.streamRegistered) {
+      // return redirect('/register');
+    } else {
+      console.log('[Middleware] User already registered on Stream backend: ', auth.userId);
+    }
+
     return NextResponse.next();
   }
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/(api|trpc)(.*)']
 };
