b127ef669470e409ead8fcaa5e9ff34d1c3973c7
Krasimir Mladenov <kdmladenov@yahoo.com>
2024-06-14 05:56:09 +0300

    Implemented useContext store and provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import { DiscordContextProvider } from '@/contexts/DiscordContext';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <DiscordContextProvider>
+          <body className={inter.className}>{children}</body>
+        </DiscordContextProvider>
       </html>
     </ClerkProvider>
   );
