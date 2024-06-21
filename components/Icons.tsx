bb160a2aeb6dd7fed3829662aba305ad88e06745
Krasimir Mladenov <kdmladenov@yahoo.com>
2024-06-21 19:06:44 +0300

    Implement CreateServerForm

---
+++ components/Icons.tsx
@@ -0,0 +1,46 @@
+import { Icon } from 'next/dist/lib/metadata/types/metadata-types';
+
+type IconProps = {
+  className?: string;
+};
+
+export function CloseIcon({
+  className = 'h-5 w-5 text-gray-500',
+}: IconProps): JSX.Element {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M6 18 18 6M6 6l12 12'
+      />
+    </svg>
+  );
+}
+
+
+export function PersonIcon({ className = 'h-8 w-8' }: IconProps): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+      />
+    </svg>
+  );
+}
